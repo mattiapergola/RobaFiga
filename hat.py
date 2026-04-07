d8bd2c030cd06068852d98062ec687c659149716
mattiapergola <mattia.pergola@mail.polimi.it>
2026-04-07 20:47:32 +0200

    Update hat.py

--- hat.py
+++ hat.py
@@ -10,7 +10,7 @@ import time
 import html
 from urllib.parse import urljoin
 
-MFPLINK = "https://scemopalle-prossiunhind.hf.space"
+MFPLINK = "https://mfp-normalissimo.onrender.com"
 MFPPSW = "Milito22."
 # Funzioni dal tuo mpd_decoder.py
 def extract_mpd_link_from_page(url):
