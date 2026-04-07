f5e1ac9d666bf9ddbf88b5293965ef586b895cae
mattiapergola <mattia.pergola@mail.polimi.it>
2026-04-07 18:04:51 +0200

    Update extract_schedule.py

--- extract_schedule.py
+++ extract_schedule.py
@@ -114,7 +114,7 @@ def extract_schedule_container(max_retries=3, retry_delay=5):
                 page.wait_for_timeout(10000)  # 10 secondi
 
                 schedule_content = page.evaluate("""() => {
-                    const container = document.getElementByClassName("schedule schedule--compact");
+                    const container = docuemnt.find("div", {"class": "schedule__day"});
                     return container ? container.outerHTML : '';
                 }""")
 
