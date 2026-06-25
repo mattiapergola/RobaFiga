42b933ebd124fdd520f35e4f5d961663fcd239c7
mattiapergola <mattia.pergola@mail.polimi.it>
2026-06-25 19:20:09 +0200

    Update MFPLINK to new proxy URL

--- hat.py
+++ hat.py
@@ -10,7 +10,7 @@ import time
 import html
 from urllib.parse import urljoin
 
-MFPLINK = "https://easyproxymiopersonale.dpdns.org"
+MFPLINK = "https://proxyfacilissimo.dpdns.org"
 MFPPSW = "Milito22."
 # Funzioni dal tuo mpd_decoder.py
 def extract_mpd_link_from_page(url):
